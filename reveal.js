257f6380764ae8500bf9a0e92759b93a204af695
Gabriel De Almeida Rocha <149016972+gabrielpyxp@users.noreply.github.com>
2025-03-24 11:37:00 -0300

    subindo site

---
+++ reveal.js
@@ -0,0 +1,104 @@
+window.revelar = ScrollReveal({reset:true})
+
+// topo do site
+
+revelar.reveal('.efeito-topo',
+    {
+        duration:1500,
+        distance:'90px'
+        
+})
+
+revelar.reveal('.efeito-topoimg',
+{
+    duration:1500,
+    distance:'90px'
+    ,delay: 500
+        
+
+})
+
+revelar.reveal('.titulos',
+    {
+        duration:1500,
+        distance:'50px'
+        
+
+})
+
+revelar.reveal('.msg',
+    {
+        duration:1500,
+        distance:'50px'
+        
+
+})
+
+
+// sessao das box
+
+revelar.reveal('.efeito-box1',
+    {
+        duration:1500,
+        distance:'50px'
+        ,delay: 300 
+
+})
+revelar.reveal('.efeito-box2',
+{
+    duration:1500,
+    distance:'50px'
+    ,delay: 600 
+        
+
+})
+revelar.reveal('.efeito-box3',
+    {
+        duration:1500,
+        distance:'50px'
+        ,delay: 900 
+})
+revelar.reveal('.efeito-box4',
+    {
+        duration:1500,
+        distance:'50px'
+        ,delay: 1200 
+
+})
+revelar.reveal('.efeito-box5',
+    {
+        duration:1500,
+        distance:'50px'
+        ,delay: 1500 
+
+})
+
+//sessao box de contato
+revelar.reveal('.efeito1',
+    {
+        duration:1500,
+        distance:'50px'
+        ,delay: 200 
+
+})
+revelar.reveal('.efeito2',
+    {
+        duration:1500,
+        distance:'50px'
+        ,delay: 500 
+
+})
+revelar.reveal('.efeito3',
+    {
+        duration:1500,
+        distance:'50px'
+        ,delay: 700 
+
+})
+revelar.reveal('.efeito4',
+    {
+        duration:1500,
+        distance:'50px'
+        ,delay: 900 
+
+})
